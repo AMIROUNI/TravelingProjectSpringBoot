abf18027b1f97b3b0a191e9acd2d8308e6245d46
Amir <amirouni162@gmail.com>
2024-11-22 10:19:28 +0100

    created the entity client

---
+++ src/main/java/org/example/travlingprojetsb/TravlingProjetSbApplication.java
@@ -0,0 +1,13 @@
+package org.example.travlingprojetsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlingProjetSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravlingProjetSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/travlingprojetsb/TravlingProjetSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.travlingprojetsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravlingProjetSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
